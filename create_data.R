841e627b20b5bc8ee0715a61606736f748448496
Roger Filmyer <roger.filmyer@gmail.com>
2018-05-11 17:32:02 -0400

    maps working at first crack

--- create_data.R
+++ create_data.R
@@ -14,7 +14,43 @@ us_contagious_diseases$cases_per_100k <- 100000 *
   us_contagious_diseases$count / 
   us_contagious_diseases$population
 
-
-us_contagious_diseases %>%
+eight_year_smallpox <- us_contagious_diseases %>%
   filter(disease == "Smallpox") %>%
-  write.csv("data/smallpox.csv")
+  group_by(state, (year) %/% 8) %>%
+  summarize(start_year = min(year), cases_per_1m = 1000000 * sum(count) / sum(population))
+
+library(geojsonio)
+states <- geojson_read("data/us-states.geojson", what = "sp")
+
+smallpox_1928_geojson <- states
+smallpox_1936_geojson <- states
+smallpox_1944_geojson <- states
+smallpox_1952_geojson <- states
+
+smallpox_1928_geojson <- sp::merge(smallpox_1928_geojson, 
+                                   eight_year_smallpox[eight_year_smallpox$start_year == 1928, 
+                                                       c("state", "start_year", "cases_per_1m")], 
+                                   by.x = "NAME", by.y = "state", all.x = TRUE)
+
+smallpox_1936_geojson <- sp::merge(smallpox_1936_geojson, 
+                                   eight_year_smallpox[eight_year_smallpox$start_year == 1936, 
+                                                       c("state", "start_year", "cases_per_1m")], 
+                                   by.x = "NAME", by.y = "state", all.x = TRUE)
+
+smallpox_1944_geojson <- sp::merge(smallpox_1944_geojson, 
+                                   eight_year_smallpox[eight_year_smallpox$start_year == 1944, 
+                                                       c("state", "start_year", "cases_per_1m")], 
+                                   by.x = "NAME", by.y = "state", all.x = TRUE)
+
+smallpox_1952_geojson <- sp::merge(smallpox_1952_geojson, 
+                                   eight_year_smallpox[eight_year_smallpox$start_year == 1952, 
+                                                       c("state", "start_year", "cases_per_1m")], 
+                                   by.x = "NAME", by.y = "state", all.x = TRUE)
+
+geojson_write(smallpox_1928_geojson, geometry = "polygon", file = "data/smallpox_1928.geojson")
+
+geojson_write(smallpox_1936_geojson, geometry = "polygon", file = "data/smallpox_1936.geojson")
+
+geojson_write(smallpox_1944_geojson, geometry = "polygon", file = "data/smallpox_1944.geojson")
+
+geojson_write(smallpox_1952_geojson, geometry = "polygon", file = "data/smallpox_1952.geojson")
